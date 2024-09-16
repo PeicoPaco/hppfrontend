7153a78290736d9e3ceca4a950bd7c149d27c262
PeicoPaco <rodrigo.reategui.g@gmail.com>
2024-09-15 19:18:24 -0500

    routes working

---
+++ frontend/src/app/requests/createrequest/createrequest.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createrequest',
+  standalone: true,
+  imports: [],
+  templateUrl: './createrequest.component.html',
+  styleUrl: './createrequest.component.css'
+})
+export class CreaterequestComponent {
+
+}
---
+++ frontend/src/app/staff/createstaff/createstaff.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-createstaff',
+  standalone: true,
+  imports: [],
+  templateUrl: './createstaff.component.html',
+  styleUrl: './createstaff.component.css'
+})
+export class CreatestaffComponent {
+
+}
