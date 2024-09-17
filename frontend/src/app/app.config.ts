211a967b867e6ecef4b7a925aa0353eef7bee4a4
PeicoPaco <rodrigo.reategui.g@gmail.com>
2024-09-17 10:47:31 -0500

    login working

--- frontend/src/app/app.config.ts
+++ frontend/src/app/app.config.ts
@@ -3,13 +3,14 @@ import { provideRouter } from '@angular/router';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { routes } from './app.routes';
 import { provideStore } from '@ngrx/store';
+import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 // import { authInterceptor } from './auth/auth.interceptor';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideStore(),
+    provideStore(), provideAnimationsAsync(),
     // provideHttpClient(withInterceptors([authInterceptor])),
   ],
 };
